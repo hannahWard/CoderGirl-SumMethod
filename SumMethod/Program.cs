8e14930a5f4358e3d764c27de97c1876fb3bc9b9
h.c.ward1991@gmail.com <h.c.ward1991@gmail.com>
2019-02-28 18:55:23 -0600

    Test Issue

--- SumMethod/Program.cs
+++ SumMethod/Program.cs
@@ -6,9 +6,21 @@ namespace SumMethod
     {
         public static void Main()
         {
+
+            Console.WriteLine("Please, choose a number: ");
+            int n = int.Parse(Console.ReadLine());
+            int total = 0;
+
+            int sum = SumMethod(total, n);
+            Console.WriteLine($"Total: {sum}");
             Console.ReadLine();
         }
 
+        public static int SumMethod(int total, int n)
+        {
+            total = (n * (n + 1) / 2);
+            return total; 
+        }
         // TODO: Create a method that returns the sum of all integer numbers up to and including given value. 
         // Use the equation (n * (n + 1)) / 2.
     }
